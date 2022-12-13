95edd83f134018c95428c58f0af349c4d7fd4fc3
YoungHa Kim <ytok1108@aol.com>
2022-12-14 00:54:05 +0900

    ChatGPT_test

---
+++ Kotlin_Lang/Data_Structures/data_structure1/algorithm.kt
@@ -0,0 +1,17 @@
+// Define a data structure to hold the input data
+data class InputData(val values: List<Int>)
+
+// Define a function to implement the algorithm
+fun algorithm(input: InputData): Int {
+    // Implement the algorithm here
+    return 0
+}
+
+// Use the function to solve the problem
+
+fun main () {
+  val result = algorithm(InputData(listOf(1, 2, 3, 4, 5)))
+
+  println(result)
+
+}
